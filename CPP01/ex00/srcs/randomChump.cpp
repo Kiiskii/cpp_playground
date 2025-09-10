57c6e2ed2580d83fe736259393ddd3e844bebcc3
Anton <112481379+Kiiskii@users.noreply.github.com>
2025-09-10 15:08:21 +0300

    Delete CPP01 directory

--- CPP01/ex00/srcs/randomChump.cpp
+++
@@ -1,6 +0,0 @@
-#include "../inc/Zombie.hpp"
-
-void randomChump(std::string name) {
-	Zombie teppo(name);
-	teppo.announce();
-}
--- CPP01/ex03/srcs/HumanA.cpp
+++
@@ -1,8 +0,0 @@
-#include "../inc/HumanA.hpp"
-
-HumanA::HumanA(const std::string& name, Weapon& weapon) : _name(name), _weapon(weapon) {
-}
-
-void HumanA::attack() {
-	std::cout << _name << " attacks with their " << _weapon.getType() << std::endl;
-}
