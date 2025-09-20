4ba01848740c7844482af0dce9b4602eb6f39b26
Anton Kiiski <akiiski@c2r1p8.hive.fi>
2025-09-20 14:44:14 +0300

    delete tests for old exercises

--- cpp03/ex01/srcs/main.cpp
+++ cpp03/ex01/srcs/main.cpp
@@ -1,35 +1,8 @@
 #include "ScavTrap.hpp"
 
 int main(void) {
-	std::cout << "\n-------------- TEST CONSTRUCTORS -------------\n" << std::endl;
-	ClapTrap clap_zero;
-	ClapTrap clap_one("One");
-	ClapTrap clap_two(clap_one);
-	ClapTrap clap_three;
-	clap_three = clap_one;
-	ClapTrap clap_sm("Someone");
+	std::cout << "\n-------------- SCAVTRAP TEST -------------\n" << std::endl;
 
-	std::cout << "\n----------- TEST ATTACK AND ENERGY -----------\n" << std::endl;
-	for (int i = 0; i < 16; i++) {
-		std::cout << i + 1 << ": ";
-		clap_one.attack("Someone");
-	}
-
-	std::cout << "\n---------------- TEST DAMAGE -----------------\n" << std::endl;
-	clap_sm.takeDamage(0);
-	clap_sm.takeDamage(2);
-	clap_sm.takeDamage(3);
-	clap_sm.takeDamage(10);
-	clap_sm.takeDamage(10);
-
-	std::cout << "\n---------------- TEST REPAIR -----------------\n" << std::endl;
-	clap_one.beRepaired(10);
-	clap_sm.beRepaired(10);
-	clap_zero.beRepaired(10);
-
-	std::cout << "\n-------------- SCAVTRAP TEST -------------" << std::endl;
-
-	std::cout << "\n-------------- TEST CONSTRUCTORS -------------\n" << std::endl;
 	ScavTrap scav_zero;
 	ScavTrap scav_one("scav_One");
 	ScavTrap scav_two(scav_one);
@@ -38,7 +11,7 @@ int main(void) {
 	ScavTrap scav_sm("scav_Someone");
 
 	std::cout << "\n----------- TEST ATTACK AND ENERGY -----------\n" << std::endl;
-	for (int i = 0; i < 16; i++) {
+	for (int i = 0; i < 52; i++) {
 		std::cout << i + 1 << ": ";
 		scav_one.attack("Someone");
 	}
