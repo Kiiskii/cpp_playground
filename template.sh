fc64180cf3b85a9a570b4f4760a97506acd491ae
Anton Kiiski <akiiski@c2r1p1.hive.fi>
2025-10-06 19:07:05 +0300

    edit template.sh to put file straight into inc and srcs dirs

--- template.sh
+++ template.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-cat TEMPLATE.cpp | sed -e "s/TEMPLATE/$1/g" > $1.cpp
-cat TEMPLATE.hpp | sed -e "s/TEMPLATE/$1/g" > $1.hpp
+# Check for argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 ClassName"
+    exit 1
+fi
+
+# Create files in srcs and inc
+cat ../../TEMPLATE.cpp | sed -e "s/TEMPLATE/$1/g" > srcs/$1.cpp
+cat ../../TEMPLATE.hpp | sed -e "s/TEMPLATE/$1/g" > inc/$1.hpp
+
+echo "Created srcs/$1.cpp and inc/$1.hpp"
