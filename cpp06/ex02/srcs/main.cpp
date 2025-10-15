72a171eb67369fef6c09578480dcafc63c667356
Anton Kiiski <akiiski@c2r1p1.hive.fi>
2025-10-15 14:02:02 +0300

    complete cpp06 ex02

--- cpp06/ex02/srcs/main.cpp
+++ cpp06/ex02/srcs/main.cpp
@@ -6,23 +6,88 @@
 #include <ctime>
 #include <exception>
 
-Base* generate() {
-	Base* ptr = nullptr;
+#define C_G		"\033[0;32m\001"
+#define C_R		"\033[0;31m\001"
+#define C_RST	"\033[0m\002"
 
-	std::srand(time(0));
-	int randomValue = (std::rand() % 100) % 3;
-	switch (randomValue) {
-		case 0:
-			ptr = new A();
-			break ;
-		case 1:
-			ptr = new B();
-			break ;
-		case 2:
-			ptr = new C();
-			break ;
-		default:
-			break ;
+Base* generate() {
+	switch (std::rand() % 3) {
+		case 0: 
+			std::cout << "creating A" << std::endl;
+			return new A();
+		case 1: return new B();
+			std::cout << "creating B" << std::endl;
+		default: 
+			std::cout << "creating C" << std::endl;
+			return new C();
 	}
-	return ptr;
+}
+
+void identify(Base* p) {
+	if (!p)
+		std::cout << "nullptr" << std::endl;
+	else if (dynamic_cast<A*>(p))
+		std::cout << "A" << C_RST << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << C_RST << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << C_RST << std::endl;
+	else
+		std::cout << C_R << "Unknown" << C_RST << std::endl;
+}
+
+void identify(Base& p) {
+	try {
+		(void)dynamic_cast<A&>(p);
+		std::cout << "A" << C_RST << std::endl;
+		return ;
+	}
+	catch (...) {
+	}
+	try {
+		(void)dynamic_cast<B&>(p);
+		std::cout << "B" << C_RST << std::endl;
+		return ;
+	}
+	catch (...) {
+	}
+	try {
+		(void)dynamic_cast<C&>(p);
+		std::cout << "C" << C_RST << std::endl;
+		return ;
+	}
+	catch (...) {
+	}
+	std::cout << C_R << "Unknown" << C_RST << std::endl;
+}
+
+int main() {
+	std::srand(time(0));
+
+	int countA = 0, countB = 0, countC = 0;
+
+	for (int i = 0; i < 10; i++) {
+		Base* b = generate();
+
+		std::cout << C_G << "pointer is: ";
+		identify(b);
+
+		if (b) {
+		std::cout << C_G << "reference is: ";
+		identify(*b);
+		}
+
+		if (dynamic_cast<A*>(b))
+			countA++;
+		else if (dynamic_cast<B*>(b))
+			countB++;
+		else if (dynamic_cast<C*>(b))
+			countC++;
+
+		delete b;
+
+		std::cout << std::endl;
+	}
+
+	std::cout << C_G << "Totals -> A: " << countA << ", B: " << countB << ", C: " << countC << C_RST << std::endl;
 }
