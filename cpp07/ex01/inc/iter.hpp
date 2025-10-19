8fbab5321a38186f51ad0fb735313e239ab6b0db
Anton Kiiski <akiiski@c1r1p8.hive.fi>
2025-10-19 16:32:33 +0300

    complete cpp07 ex01

---
+++ cpp07/ex01/inc/iter.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+
+#define C_R		"\033[0;31m\001"
+#define C_G		"\033[0;32m\001"
+# define C_RST	"\033[0m\002"
+
+template <typename T, typename F>
+void iter(T* arr, size_t len, F func) {
+	if (!arr || !len)
+		return ;
+	for (size_t i = 0; i < len; i++)
+		func(arr[i]);
+}
